97c42d37bb533ced6c204c3ebca90d46969e8d12
Tejavengaladasu <96474305+Tejavengaladasu@users.noreply.github.com>
2022-11-02 14:49:45 +0530

    import java.util.*; class teja{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         float m=s.nextFloat();         float n=s.nextFloat();         float avg=(m+n)/2;         System.out.printf("%.4f",avg);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class teja{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        float m=s.nextFloat();
+        float n=s.nextFloat();
+        float avg=(m+n)/2;
+        System.out.printf("%.4f",avg);
+    }
+}
