5514132935d71fb8d9a870c130f47e9d60a050f5
Tejavengaladasu <96474305+Tejavengaladasu@users.noreply.github.com>
2022-11-02 15:02:09 +0530

    import java.util.*; class teja{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         int m=s.nextInt();         int n=s.nextInt();         System.out.println(m/n);     } }

---
+++ Division.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class teja{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int m=s.nextInt();
+        int n=s.nextInt();
+        System.out.println(m/n);
+    }
+}
